2b46f31f4909e473a5b04d17b2c7bb77aa36cf70
Suhaas D <suhasdgupta55555@gmail.com>
2021-03-13 21:31:07 +0530

    Update BinaryTreesWithFactors.java

--- BinaryTreesWithFactors.java
+++ BinaryTreesWithFactors.java
@@ -1,14 +1,14 @@
 class BinaryTreesWithFactors {
     public int numFactoredBinaryTrees(int[] arr) {
         long res = 0L, mod = (long)1e9 + 7;
-        Arrays.sort(A);
+        Arrays.sort(arr);
         HashMap<Integer, Long> dp = new HashMap<>();
-        for (int i = 0; i < A.length; ++i) {
-            dp.put(A[i], 1L);
+        for (int i = 0; i < arr.length; ++i) {
+            dp.put(arr[i], 1L);
             for (int j = 0; j < i; ++j)
-                if (A[i] % A[j] == 0)
-                    dp.put(A[i], (dp.get(A[i]) + dp.get(A[j]) * dp.getOrDefault(A[i] / A[j], 0L)) % mod);
-            res = (res + dp.get(A[i])) % mod;
+                if (arr[i] % arr[j] == 0)
+                    dp.put(arr[i], (dp.get(arr[i]) + dp.get(arr[j]) * dp.getOrDefault(arr[i] / arr[j], 0L)) % mod);
+            res = (res + dp.get(arr[i])) % mod;
         }
         return (int) res;
     }
